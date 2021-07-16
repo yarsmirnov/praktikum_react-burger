4c4abef4ceded9734774b29506fd1dcbbde556cb
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-07-16 17:12:13 +0300

    feat: Add total to BurgerConstructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,6 +1,17 @@
-import { ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { ConstructorElement, Button, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerConstructorStyles from './burger-constructor.module.css';
 
+
+const getTotal = (bun, fillings) => {
+  let result = 0;
+
+  result += bun.price * 2;
+  result += fillings.reduce((acc, item) => {
+    return acc += item.price;
+  }, result);
+
+  return result;
+};
 
 const generateCornerBun = (bun, type = 'top') => {
   const bunType = type === 'bottom' ? type : 'top';
@@ -43,6 +54,7 @@ const generateFillings = (items) => {
 const BurgerConstructor = ({ ingredients }) => {
   const cornerBun = ingredients[0];
   const fillings = ingredients.slice(1);
+  const total = getTotal(cornerBun, fillings);
 
   return (
     <section className={`${BurgerConstructorStyles.section} column pt-25`}>
@@ -63,9 +75,26 @@ const BurgerConstructor = ({ ingredients }) => {
       </div>
 
       <div
-        className={BurgerConstructorStyles.bunBotton}
+        className={`${BurgerConstructorStyles.bunBotton} mb-10`}
       >
         {generateCornerBun(cornerBun, 'bottom')}
+      </div>
+
+      <div
+        className={BurgerConstructorStyles.order}
+      >
+        <span
+          className={`${BurgerConstructorStyles.orderTotal} text_type_digits-medium mr-10`}
+        >
+          {total}
+          <i className={`${BurgerConstructorStyles.orderCurrencyIcon} ml-2`}>
+            <CurrencyIcon type="primary" />
+          </i>
+        </span>
+
+        <Button type="primary" size="large">
+          Оформить заказ
+        </Button>
       </div>
     </section>
   );
