49a7974d2c7cca0601768b6c9a18b5c1a06a1308
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-08-12 10:22:52 +0300

    refactor: Replace app's store to a separated file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './services/reducers';
+import store from './services/store';
 
 import App from './components/app/app';
 
 import './index.css';
 
-
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: [thunk],
-  devTools: process.env.NODE_ENV !== 'production',
-});
 
 ReactDOM.render(
   <Provider store={store}>
