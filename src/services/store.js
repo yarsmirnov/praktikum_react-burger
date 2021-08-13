41f229392554dbc7508bd3dd2797bc8d7f0af12f
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-08-13 15:32:44 +0300

    ci: Use human-friendly services naming and architecture

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
+import rootReducer from './reducers/root-reducer';
 
 const store = configureStore({
   reducer: rootReducer,
