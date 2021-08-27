b774a0e14620f303ab1224413db4fdd370893c88
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-08-27 14:33:36 +0300

    feat: Add Router Links to PageHeader for Login and Profile pages

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 
 const AppHeader = () => {
+  const { user } = useSelector(store => store.auth);
+
   return (
     <header className={`${styles.header} pt-4 pb-4`}>
       <nav className={`${styles.appNavigation} text_type_main-default`}>
@@ -25,17 +29,32 @@ const AppHeader = () => {
         </li>
       </nav>
 
-      <a href="#nowhere">
+      <Link to={'/'}>
         <Logo />
-      </a>
+      </Link>
 
       <ul className={`${styles.userMenu} text_type_main-default`}>
         <li className='pt-4 pb-4 pl-5 pr-5 text_color_inactive'>
+          { user
+            ? (
+              <Link to={'/profile'} className={styles.link}>
+                <i className='mr-2'>
+                  <ProfileIcon type="secondary" />
+                </i>
+                Личный кабинет
+              </Link>
+            )
+            : (
+              <Link to={'/login'} className={styles.link}>
+                <i className='mr-2'>
+                  <ListIcon type="secondary" />
+                </i>
+                Войти
+              </Link>
+            )
+          }
+
           <a className={styles.link} href="#nowhere">
-            <i className='mr-2'>
-              <ProfileIcon type="secondary" />
-            </i>
-            Личный кабинет
           </a>
         </li>
       </ul>
