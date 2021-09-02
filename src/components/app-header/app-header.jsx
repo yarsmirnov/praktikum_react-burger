e72fdb04500e1dc31fdf7cc719784d0151ff9a1a
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-09-02 14:02:11 +0300

    refactor: Replace Header navigation <a> with <Link>

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -12,12 +12,12 @@ const AppHeader = () => {
     <header className={`${styles.header} pt-4 pb-4`}>
       <nav className={`${styles.appNavigation} text_type_main-default`}>
         <li className='mr-2 pt-4 pb-4 pl-5 pr-5'>
-          <a className={styles.link} href="#nowhere">
+          <Link to='/' className={styles.link}>
             <i className='mr-2'>
               <BurgerIcon type="primary" />
             </i>
             Конструктор
-          </a>
+          </Link>
         </li>
         <li className='pt-4 pb-4 pl-5 pr-5 text_color_inactive'>
           <a className={styles.link} href="#nowhere">
