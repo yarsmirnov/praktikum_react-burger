20f907800a71fb243d6802b9b06398eab499014b
Yaroslav Smirnov <yar14.dev@gmail.com>
2021-09-30 14:31:14 +0300

    refactor: Typing NotFound404 component

--- src/pages/not-found-404 .tsx
+++ src/pages/not-found-404 .tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 import layoutStyles from './page-layout.module.css';
 
 
-export const NotFound404 = () => {
+export const NotFound404: FC<{}> = () => {
   return (
     <div className={`${layoutStyles.pageContainer} text-center pt-30`}>
       <h1 className={`text text_type_main-large mb-20`}>
